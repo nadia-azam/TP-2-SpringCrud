b9ba88a9fef812ad40fd0e2bce3c811e2c593485
nadia-azam <azam.nadia.2002@gmail.com>
2024-11-08 21:39:17 +0100

    second commit

--- src/main/java/com/example/TP2/models/Task.java
+++ src/main/java/com/example/TP2/models/Task.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -20,11 +24,15 @@ public class Task {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
+
+    @NotBlank
+    @Size(max = 255)
     private String description ;
 
-    @DateTimeFormat
+    @NotNull
     private Date date_create ;
 
     @DateTimeFormat
+    @NotNull
     private Date date_echenace ;
 }
